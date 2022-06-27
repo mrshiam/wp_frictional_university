ab79125fd9aff85ab01013ddb136abb6acb78d4e
mrshiam <mrshiam01@gmail.com>
2022-06-27 18:05:19 +0600

    custom blog page

--- wp-content/themes/frictional-university-theme/single.php
+++ wp-content/themes/frictional-university-theme/single.php
@@ -1,7 +1,38 @@
 <?php 
     get_header(); ?>
 
-    <p>Hi</p>
+    <?php
+        while(have_posts()){
+        the_post();
+    ?>
+        <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title(); ?></h1>
+                <div class="page-banner__intro">
+                <p>Keep up with our latest news</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                <a class="metabox__blog-home-link" href="">
+                <i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> On <?php the_time('F'); ?> in News</span>
+                </p>
+            </div>
+            <div class="generic-content">
+                  <?php the_excerpt(); ?>
+                 
+                </div>
+        </div>
+    <?php    }
+    
+    
+    
+    ?>
 <?php 
     get_footer();
 

46d176a5b3e9432df4c22783af1ba1a70fe57f99
mrshiam <mrshiam01@gmail.com>
2022-06-28 16:05:45 +0600

    displaying custom post type

--- wp-content/themes/frictional-university-theme/single-event.php
+++ wp-content/themes/frictional-university-theme/single-event.php
@@ -19,8 +19,8 @@
         <div class="container container--narrow page-section">
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
-                <a class="metabox__blog-home-link" href="">
-                <i class="fa fa-home" aria-hidden="true"></i> Blog Home </a> <span class="metabox__main">Posted by <?php the_author_posts_link(); ?> On <?php the_time('F'); ?> in News</span>
+                <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event') ?>">
+                <i class="fa fa-home" aria-hidden="true"></i> Events Home </a> <span class="metabox__main"><?php the_title(); ?></span>
                 </p>
             </div>
             <div class="generic-content">

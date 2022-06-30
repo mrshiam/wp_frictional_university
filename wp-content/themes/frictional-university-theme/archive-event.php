a432672056e8d43ed46f83f2cc4fa6dfa7aa2923
mrshiam <mrshiam01@gmail.com>
2022-06-30 18:36:03 +0600

    reuse code for repeat code

--- wp-content/themes/frictional-university-theme/archive-event.php
+++ wp-content/themes/frictional-university-theme/archive-event.php
@@ -1,13 +1,10 @@
-<?php get_header(); ?>
-        <div class="page-banner">
-            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
-            <div class="page-banner__content container container--narrow">
-                <h1 class="page-banner__title">All Events</h1>
-                <div class="page-banner__intro">
-                <p>See what is going in our world</p>
-                </div>
-            </div>
-        </div>
+<?php get_header(); 
+  pageBanner(array(
+    'title' => 'All Events',
+    'subtitle' => 'See what is going in our world'
+  ));
+?>
+        
 
 
         <div class="container container--narrow page-section">
@@ -35,7 +32,7 @@
 
            <hr class="section-break">
 
-           <p>Looking for a recap of past events? <a href="<?php site_url('/past-events') ?>">Check out our past events archive.</a></p>
+           <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events') ?>">Check out our past events archive.</a></p>
         </div>
 
 <?php get_footer(); ?>
--- wp-content/themes/frictional-university-theme/functions.php
+++ wp-content/themes/frictional-university-theme/functions.php
@@ -1,4 +1,36 @@
 <?php
+
+function pageBanner($args = NULL){ 
+
+    if (!$args['title']){
+        $args['title'] = get_the_title();
+    }
+
+    if (!$args['subtitle']){
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
+    if (!$args['photo']){
+        if (get_field('page_banner_image')){
+            $args['photo'] = get_field('page_banner_image')['sizes']['bannerImage'];
+        
+    }else{
+        $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+}
+    ?>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php echo $args['title'];  ?></h1>
+            <div class="page-banner__intro">
+            <p><?php echo $args['subtitle']; ?></p>
+            </div>
+        </div>
+    </div>
+<?php }
+
+
 function university_files(){
     wp_enqueue_script('university-main-js', get_theme_file_uri('/js/index.js'),NULL, microtime(), true);
     wp_enqueue_style('custom-google-font','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -14,6 +46,7 @@ function university_features(){
     add_theme_support('post-thumbnails');
     add_image_size('professorLandscape', 400, 260, true);
     add_image_size('professorPotrait', 480, 650, true);
+    add_image_size('bannerImage', 1500, 350, true);
 }
 add_action('after_setup_theme','university_features');
 
